738fa58e959bc8fcbc62570b21fb58f923676631
ivan larios pagodiario <robotdemn@gmail.com>
2022-02-20 19:19:29 -0600

    secondsession

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as components from './components';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
   declarations: [
     ...components.components
   ],
-  imports: [
-    CommonModule
-  ],
   exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     ...components.components
   ]
 })
